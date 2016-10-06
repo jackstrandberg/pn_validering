f8d41e166af7ed6160d0c2d71377e3a05437c7c7
jackstrandberg <jack.berg.768@gmail.com>
2016-10-06 19:57:40 +0200

    Valid personNumber for Long test. formatting

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+package validering;
 
 public class Main {
 
@@ -9,15 +10,17 @@ public class Main {
         ValidityCheck isPersonNumber = new IsPersonNumber();
         ValidityCheck notNull = new NotNull();
 
-        /* Create ValidityChecker */
         ValidityCheck[] checkObjects = {isPersonNumber, notNull};
+
+
+        /* Create ValidityChecker */
         ValidityChecker checker = new ValidityChecker(checkObjects);
 
-
+        
         checker.check("190103203451");    // pass
         checker.check("0103203451");      // pass
         checker.check("190103203452");    // fail, not correct control digit
-        checker.check(190103203452L);     // fail, not a string
+        checker.check(190103203451L);     // fail, not a string
         checker.check(null);              // fail
         checker.check();                  // no check performed
     }
