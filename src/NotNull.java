7a841d9c5d8d94aea7cec6e81990d1e4b05c8604
jackstrandberg <jack.berg.768@gmail.com>
2016-10-07 00:13:16 +0200

    rm duplicate

--- src/NotNull.java
+++ src/NotNull.java
@@ -3,11 +3,7 @@ public class NotNull implements ValidityCheck {
 
     @Override
     public boolean check(Object candidateData) {
-            return notNull(candidateData);
-    }
-
-    private boolean notNull(Object candidateData) {
-            return (candidateData != null);
+        return (candidateData != null);
     }
 
     @Override
